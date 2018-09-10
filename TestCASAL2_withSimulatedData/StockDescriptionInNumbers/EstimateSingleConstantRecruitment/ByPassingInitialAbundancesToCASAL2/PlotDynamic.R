266811c652031d08a0166a499b03ecdf2e97d306
Marco Kienzle <marco.kienzle@gmail.com>
2018-09-11 10:11:05 +1200

    Added Latex/Beamer presentation

--- TestCASAL2_withSimulatedData/StockDescriptionInNumbers/EstimateSingleConstantRecruitment/ByPassingInitialAbundancesToCASAL2/PlotDynamic.R
+++ TestCASAL2_withSimulatedData/StockDescriptionInNumbers/EstimateSingleConstantRecruitment/ByPassingInitialAbundancesToCASAL2/PlotDynamic.R
@@ -1,5 +1,5 @@
 # CREATED  10 Sep 2018
-# MODIFIED 10 Sep 2018
+# MODIFIED 11 Sep 2018
 
 # PURPOSE give a visual representation of the dynamic of number
 
@@ -24,13 +24,17 @@ X <- seq(x[length(x)] + 1, length = length(Y0))
 postscript(file = "Results/Graphics/PopDynamics.ps")
 pchar <- 19
 
-plot(x, y0, xlab = "", ylab = "Abundance (nb)", las = 1, xlim = c(1985, 2010), ylim = c(min(Y3), max(y0)), pch = pchar)
+# parameters for a beamer presentation
+par(mgp = c(4,1,0), cex.lab = 1.5, cex = 1.2, mai = c(1.02, 1.5, 0.82, 0.42))
+plot(x, y0, xlab = "", ylab = "Abundance (nb)", las = 1,
+xlim = c(1985, 2010), ylim = c(min(Y3), max(y0)), pch = pchar)
+
 points(x + 0.5, y1, pch = pchar)
 points(x + 0.5, y2, pch = pchar)
 points(x + 1, y3, pch = pchar)
 for(i in 1:length(x)){
       #print(x[i])
-      curve(y0[i] * exp(-sim$preExploitationPop$M * (t-x[i])), from = x[i], to = x[i]+1, xname = "t", add = TRUE, col = "green", lwd = 2.0)
+      curve(y0[i] * exp(-sim$preExploitationPop$M * (t-x[i])), from = x[i], to = x[i]+1, xname = "t", add = TRUE, col = "blue", lwd = 2.0)
       }
 segments(x[-1], y3[-length(y3)], x[-1], y0[-1])
 
@@ -41,8 +45,8 @@ points(X+0.5, Y2, pch = pchar)
 points(X+1, Y3, pch = pchar)
 
 for(i in 1:length(X)){
-      curve(Y0[i] * exp(-sim$DuringExploitationPop$M * (t-X[i])), from = X[i], to = X[i]+0.5, xname = "t", add = TRUE, col = "green", lwd = 2.0)
-      curve(Y2[i] * exp(-sim$DuringExploitationPop$M * (t-X[i]-0.5)), from = X[i] + 0.5, to = X[i]+1, xname = "t", add = TRUE, col = "green", lwd = 2.0)
+      curve(Y0[i] * exp(-sim$DuringExploitationPop$M * (t-X[i])), from = X[i], to = X[i]+0.5, xname = "t", add = TRUE, col = "blue", lwd = 2.0)
+      curve(Y2[i] * exp(-sim$DuringExploitationPop$M * (t-X[i]-0.5)), from = X[i] + 0.5, to = X[i]+1, xname = "t", add = TRUE, col = "blue", lwd = 2.0)
       }
 
 segments(X+0.5, Y1, X+0.5, Y2, col = "red", lwd = 2.0)
--- TestCASAL2_withSimulatedData/StockDescriptionInNumbers/EstimateSingleConstantRecruitment/ByPassingInitialAbundancesToCASAL2/PlotResults.R
+++ TestCASAL2_withSimulatedData/StockDescriptionInNumbers/EstimateSingleConstantRecruitment/ByPassingInitialAbundancesToCASAL2/PlotResults.R
@@ -1,6 +1,9 @@
 tmp <- read.csv("Results/SimVsEst.csv", header = FALSE, col.names = c("Simulated", "Survival.Analysis", "CASAL2"))
 
-pdf(file = "Results/Graphics/SimVsEst.pdf")
+postscript(file = "Results/Graphics/SimVsEst.ps")
+# parameters for a beamer presentation
+par(mgp = c(4,1,0), cex.lab = 1.5, cex = 1.2, mai = c(1.4, 1.5, 0.82, 0.42))
+
 with(tmp, plot(Simulated, CASAL2, pch = 3, xlab = "Simulated", cex = 2, ylab = "Estimated", las = 1))
 #with(tmp, points(Simulated, Survival.Analysis, pch = 19, cex = 0.5, col = "lightgrey"))
 #legend(0.65 * max(tmp$Simulated), 0.2 * max(tmp$Simulated), pch = c(3,19), legend = c("CASAL2", "Survival analysis"),
