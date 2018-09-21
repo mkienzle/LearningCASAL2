94beaaf1d6f7010fc3d4b487d60e1f25b419ae7d
Marco Kienzle <marco.kienzle@gmail.com>
2018-09-21 14:48:50 +1200

    CHG: PlotResults.R to plot 2 graphs seperately

--- TestCASAL2_withSimulatedData/StockDescriptionInNumbers/EstimateSingleConstantRecruitment/ByPassingInitialAbundancesToCASAL2/PlotResults.R
+++ TestCASAL2_withSimulatedData/StockDescriptionInNumbers/EstimateSingleConstantRecruitment/ByPassingInitialAbundancesToCASAL2/PlotResults.R
@@ -1,3 +1,5 @@
+# PURPOSE compare estimated recruitment with simulated
+
 tmp <- read.csv("Results/SimVsEst.csv", header = FALSE, col.names = c("Simulated", "Survival.Analysis", "Roptimize", "CASAL2"))
 
 postscript(file = "Results/Graphics/SimVsEst.ps")
@@ -6,7 +8,18 @@ par(mgp = c(4,1,0), cex.lab = 1.5, cex = 1.2, mai = c(1.4, 1.5, 0.82, 0.42))
 
 with(tmp, plot(Simulated, CASAL2, pch = 3, xlab = "Simulated", cex = 2, ylab = "Estimated", las = 1))
 #with(tmp, points(Simulated, Survival.Analysis, pch = 19, cex = 0.5, col = "lightgrey"))
-with(tmp, points(Simulated, Roptimize, pch = 5))
-legend(0.65 * max(tmp$Simulated), 0.3 * max(tmp$Simulated), pch = c(3,5), legend = c("CASAL2", "Roptimize"))
+#with(tmp, points(Simulated, Roptimize, pch = 5))
+legend(0.65 * max(tmp$Simulated), 0.3 * max(tmp$Simulated), pch = 3, legend = "CASAL2")
+abline(0,1, col = "blue")
+dev.off()
+
+postscript(file = "Results/Graphics/SimVsEst2.ps")
+# parameters for a beamer presentation
+par(mgp = c(4,1,0), cex.lab = 1.5, cex = 1.2, mai = c(1.4, 1.5, 0.82, 0.42))
+
+with(tmp, plot(Simulated, CASAL2, pch = 3, xlab = "Simulated", cex = 2, ylab = "Estimated", las = 1))
+#with(tmp, points(Simulated, Survival.Analysis, pch = 19, cex = 0.5, col = "lightgrey"))
+with(tmp, points(Simulated, Roptimize, pch = 5, col = "red"))
+legend(0.65 * max(tmp$Simulated), 0.3 * max(tmp$Simulated), pch = c(3,5), legend = c("CASAL2", "Roptimize"), col = c("black","red"))
 abline(0,1, col = "blue")
 dev.off()
